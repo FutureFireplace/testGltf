c384189e095531b6ece89d8b409beb03ad55e82b
Thomas Due Nielsen <thniel12@student.aau.dk>
2024-10-16 20:03:28 +0200

    Add files via upload

---
+++ getWeatherDt_AutmClst_mult33_44.php
@@ -0,0 +1,14 @@
+<?php
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+  $fortnitePeriods = [
+                        [
+                          "dayHourConsump"=>"-12",
+                          "windPrClust": "86",
+                          "sunPrClust": "0" ,
+                          "surplus": "-17"
+                         ]
+                    ];
+ ?>
